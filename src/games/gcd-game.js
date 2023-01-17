7783afcef6c6cca92bb1c121f4e027b63512602d
v-vberezovskiy <qedirast@gmail.co>
2023-01-17 17:21:06 +0700

    fix gcd game

--- src/games/gcd-game.js
+++ src/games/gcd-game.js
@@ -1,62 +1,61 @@
-import readlineSync from 'readline-sync'
+import readlineSync from 'readline-sync';
 
-export default name => {
-  console.log('Find the greatest common divisor of given numbers.')
+export default (name) => {
+  console.log('Find the greatest common divisor of given numbers.');
 
-  const GAME_ROUNDS = 3
+  const GAME_ROUNDS = 3;
 
-  const getRandomNumber = () => Math.round(Math.random() * 20)
+  const getRandomNumber = () => Math.round(Math.random() * 20);
 
-  const gcd = (a, b) => {
+  const gcd = (j, z) => {
+    let a = j;
+    let b = z;
+
     while (a !== 0 && b !== 0) {
       if (a > b) {
-        a = a % b
+        a %= b;
       } else {
-        b = b % a
+        b %= a;
       }
     }
 
-    return a + b
-  }
+    return a + b;
+  };
 
-  const getCorrectAnswer = (a, b) => {
-    return gcd(a, b)
-  }
+  const getCorrectAnswer = (a, b) => gcd(a, b);
 
-  const isCorrectAnswer = (correctAnswer, userAnswer) =>
-    correctAnswer === +userAnswer
+  const isCorrectAnswer = (correctAnswer, userAnswer) => correctAnswer === +userAnswer;
 
-  const getWrongAnswerMsg = (correctAnswer, userAnswer) =>
-    `'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`
+  const getWrongAnswerMsg = (correctAnswer, userAnswer) => `'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`;
 
   const gcdGame = () => {
     for (let i = 0; i < GAME_ROUNDS; i += 1) {
-      const randomNumberA = getRandomNumber()
-      const randomNumberB = getRandomNumber()
+      const randomNumberA = getRandomNumber();
+      const randomNumberB = getRandomNumber();
 
-      console.log(`Question: ${randomNumberA} ${randomNumberB}`)
+      console.log(`Question: ${randomNumberA} ${randomNumberB}`);
 
-      const userAnswer = readlineSync.question('Your answer: ')
-      const correctAnswer = getCorrectAnswer(randomNumberA, randomNumberB)
+      const userAnswer = readlineSync.question('Your answer: ');
+      const correctAnswer = getCorrectAnswer(randomNumberA, randomNumberB);
 
-      const isCorrect = isCorrectAnswer(correctAnswer, userAnswer)
+      const isCorrect = isCorrectAnswer(correctAnswer, userAnswer);
 
       const roundMessage = isCorrect
         ? 'Correct!'
-        : getWrongAnswerMsg(correctAnswer, userAnswer)
+        : getWrongAnswerMsg(correctAnswer, userAnswer);
 
       if (!isCorrect) {
-        console.log(roundMessage)
-        console.log(`Let's try again, ${name}!`)
+        console.log(roundMessage);
+        console.log(`Let's try again, ${name}!`);
 
-        return
+        return;
       }
 
-      console.log(roundMessage)
+      console.log(roundMessage);
     }
 
-    console.log(`Congratulations, ${name}!`)
-  }
+    console.log(`Congratulations, ${name}!`);
+  };
 
-  gcdGame()
-}
+  gcdGame();
+};
