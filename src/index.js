39a8393af3f3142cfacac72aaed6b9c8e390add2
v-vberezovskiy <qedirast@gmail.co>
2023-01-17 17:47:10 +0700

    progression game

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import evenGame from './games/even-game.js';
 import calcGame from './games/calc-game.js';
 import gcdGame from './games/gcd-game.js';
+import progressionGame from './games/progression-game.js';
 
 const gameSelector = {
   even: (userName) => evenGame(userName),
   calc: (userName) => calcGame(userName),
   gcd: (userName) => gcdGame(userName),
+  progression: (userName) => progressionGame(userName),
 };
 
 export default (userName, gameID) => {
